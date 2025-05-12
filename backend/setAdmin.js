9d3809ef7cafffd85143d14ae7e41fcd2c3826a8
abenomran <abdalla.benomran2@gmail.com>
2025-05-12 15:42:45 -0400

    base64 encode service account

--- backend/setAdmin.js
+++ backend/setAdmin.js
@@ -1,5 +1,10 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+// old, manually downloaded service account key
+// const serviceAccount = require("./serviceAccountKey.json");
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
